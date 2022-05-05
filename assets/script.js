dcf9a5dfda38b112b745ea974aa41f90586c9b39
David <contact@underground-rp.de>
2022-05-05 12:22:18 +0200

    added copy discord name function

--- assets/script.js
+++ assets/script.js
@@ -6,4 +6,24 @@ $.getJSON("https://dcdn.dstn.to/profile/398101340322136075", function (data) {
 		"</span><span class='h4 text-white-50 whitney'>#" +
 		data.user.discriminator +
 		"</span>";
-});
+
+	document.getElementById("buttoncopystandard").addEventListener('click', copyToClickBoard);
+	var buttoncopystandard = document.getElementById("buttoncopystandard")
+	var buttoncopydiv = document.getElementById("buttoncopy")
+	var copyText = data.user.username + data.user.discriminator;
+
+	function copyToClickBoard() {
+		navigator.clipboard.writeText(copyText)
+			.then(() => {
+				buttoncopystandard.style.display = "none"
+				buttoncopydiv.innerHTML =
+					"<span class='text-white bi bi-clipboard-check-fill'></span>";
+			})
+			.catch(err => {
+				console.log('Something went wrong', err);
+				buttoncopystandard.style.display = "none"
+				buttoncopydiv.innerHTML =
+					"<span class='text-white bi bi-clipboard-x-fill'></span>";
+			})
+	}
+})

36d699ae116882926b987946fd721e01e8b92733
David <d.huhmann34@gmail.com>
2022-05-16 13:23:56 +0200

    replaced jquery, fixed bootstrap.min.css issue

--- assets/script.js
+++ assets/script.js
@@ -1,29 +1,34 @@
-$.getJSON("https://dcdn.dstn.to/profile/398101340322136075", function (data) {
-	var discordtext = document.getElementById("discordtext");
-	discordtext.innerHTML =
-		"<span class='h4 text-white whitney'>" +
-		data.user.username +
-		"</span><span class='h4 text-white-50 whitney'>#" +
-		data.user.discriminator +
-		"</span>";
+fetch("https://dcdn.dstn.to/profile/398101340322136075")
+  .then((response) => response.json())
+  .then((data) => {
+    var discordtext = document.getElementById("discordtext");
+    discordtext.innerHTML =
+      "<span class='h4 text-white whitney'>" +
+      data.user.username +
+      "</span><span class='h4 text-white-50 whitney'>#" +
+      data.user.discriminator +
+      "</span>";
 
-	document.getElementById("buttoncopystandard").addEventListener('click', copyToClickBoard);
-	var buttoncopystandard = document.getElementById("buttoncopystandard")
-	var buttoncopydiv = document.getElementById("buttoncopy")
-	var copyText = data.user.username + "#" + data.user.discriminator;
+    document
+      .getElementById("buttoncopystandard")
+      .addEventListener("click", copyToClickBoard);
+    var buttoncopystandard = document.getElementById("buttoncopystandard");
+    var buttoncopydiv = document.getElementById("buttoncopy");
+    var copyText = data.user.username + "#" + data.user.discriminator;
 
-	function copyToClickBoard() {
-		navigator.clipboard.writeText(copyText)
-			.then(() => {
-				buttoncopystandard.style.display = "none"
-				buttoncopydiv.innerHTML =
-					"<span class='text-white bi bi-clipboard-check-fill'></span>";
-			})
-			.catch(err => {
-				console.log('Something went wrong', err);
-				buttoncopystandard.style.display = "none"
-				buttoncopydiv.innerHTML =
-					"<span class='text-white bi bi-clipboard-x-fill'></span>";
-			})
-	}
-})
+    function copyToClickBoard() {
+      navigator.clipboard
+        .writeText(copyText)
+        .then(() => {
+          buttoncopystandard.style.display = "none";
+          buttoncopydiv.innerHTML =
+            "<span class='text-white bi bi-clipboard-check-fill'></span>";
+        })
+        .catch((err) => {
+          console.log("Something went wrong", err);
+          buttoncopystandard.style.display = "none";
+          buttoncopydiv.innerHTML =
+            "<span class='text-white bi bi-clipboard-x-fill'></span>";
+        });
+    }
+  });

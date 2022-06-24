0ea50ad08fd9b3e35b78b6712eeecf2e824e6762
David <contact@underground-rp.de>
2022-06-24 19:14:20 +0200

    added social media import for connected accounts

--- assets/script.js
+++ assets/script.js
@@ -31,6 +31,85 @@ fetch("https://dcdn.dstn.to/profile/398101340322136075")
             "<span class='text-white bi bi-clipboard-x-fill'></span>";
         });
     }
+
+    var accountsdiv = document.getElementById("accountsdiv");
+    var accountLink = "";
+    let value = '';
+    let colorvalue = '';
+
+    var socialMedia = [
+      {
+        YouTube: {
+          url: "https://www.youtube.com/channel/",
+          color: "#de1f1f"
+        },
+        Spotify: {
+          url: "https://open.spotify.com/user/",
+          color: "#1DB954"
+        },
+        Twitch: {
+          url: "https://www.twitch.tv/",
+          color: "#6441a5"
+        },
+        Twitter: {
+          url: "https://twitter.com/",
+          color: "#1DA1F2"
+        },
+        Steam: {
+          url: "https://steamcommunity.com/profiles/",
+          color: "#0b6498"
+        },
+        GitHub: {
+          url: "https://github.com/",
+          color: "#FFF"
+        },
+        Reddit: {
+          url: "https://www.reddit.com/user/",
+          color: "#FF5700"
+        }
+      }
+    ]
+
+    data.connected_accounts.forEach((accounts) => {
+
+      if (accounts.type === "youtube") {
+        accountLink = socialMedia[0].YouTube.url + accounts.id;
+        colorvalue = socialMedia[0].YouTube.color;
+      }
+
+      if (accounts.type === "twitter") {
+        accountLink = socialMedia[0].Twitter.url + accounts.name;
+        colorvalue = socialMedia[0].Twitter.color;
+      }
+
+      if (accounts.type === "twitch") {
+        accountLink = socialMedia[0].Twitch.url + accounts.name;
+        colorvalue = socialMedia[0].Twitch.color;
+      }
+
+      if (accounts.type === "github") {
+        accountLink = socialMedia[0].GitHub.url + accounts.name;
+        colorvalue = socialMedia[0].GitHub.color;
+      }
+
+      if (accounts.type === "reddit") {
+        accountLink = socialMedia[0].Reddit.url + accounts.id;
+        colorvalue = socialMedia[0].Reddit.color;
+      }
+
+      if (accounts.type === "spotify") {
+        accountLink = socialMedia[0].Spotify.url + accounts.id;
+        colorvalue = socialMedia[0].Spotify.color;
+      }
+
+      if (accounts.type === "steam") {
+        accountLink = socialMedia[0].Steam.url + accounts.id;
+        colorvalue = socialMedia[0].Steam.color;
+      }
+
+      value += "<a href=" + accountLink + " target='_blank' class='mx-2' style='color: " + colorvalue + " !important'><i class='bi bi-" + accounts.type + "'></i></a>";
+    });
+    accountsdiv.innerHTML = value;
   });
 
 fetch("https://api.lanyard.rest/v1/users/398101340322136075")
